dd47312462689a0b6533ffbb6e730532790c1364
Andres <imanolrodriguez124@gmail.com>
2026-02-05 04:36:05 -0500

    Pool

---
+++ src/conexion_python_mysql/pool_conexiones.py
@@ -0,0 +1,42 @@
+from mysql.connector import pooling
+from mysql.connector import Error
+
+class Conexion:
+    database ="blogdb"
+    username ="root"
+    password = "123456"
+    host ="localhost"
+    port = 3306 #puerto por defecto
+    pool_size = 5 #cantidad de objetos
+    pool_name ="plantilla de un pool de conecciones" #nombre del pool
+    pool = None
+
+    @classmethod
+    def obtener_pool(cls):
+        if cls.pool is None: #se crea el objeto pool
+            try:
+                cls.pool = pooling.MySQLConnectionPool(
+                    pool_name = cls.pool_name,
+                    pool_size = cls.pool_size,
+                    host = cls.host,
+                    port = cls.port,
+                    database = cls.database,
+                    user = cls.username,
+                    password = cls.password
+                )
+                return cls.pool
+            except Error as e:
+                print(f"Ocurrio un error al obtener pool: {e}")
+        else:
+            return cls.pool
+    @classmethod
+    def obtener_conexion(cls):
+        return cls.obtener_pool().get_connection()
+
+    @classmethod
+    def liberar_conexion(cls,conexion):
+        conexion.close()
+
+if __name__ == "__main__":
+    pool = Conexion.obtener_pool()
+    print(pool)
